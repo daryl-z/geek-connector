4bc30fb30467a5181d54736b06f363e611229ed5
zzz <angelks4@live.com>
2018-04-16 14:00:57 +0800

    add routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 const app = express();
 
 //  数据库配置
@@ -14,6 +18,10 @@ mongoose
 // 首页路由
 app.get("/", (req, res) => res.send("Hello dfd"));
 
-const port = process.env.PORT || 5000;
+// Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Ruing On Port ${port}...`));
