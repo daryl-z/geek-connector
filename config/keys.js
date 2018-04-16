3343fafcc3b97ab994c13edf7e2b7907fc85835c
zzz <angelks4@live.com>
2018-04-16 13:47:05 +0800

    connect to mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://zzz:123456@ds054619.mlab.com:54619/geek-connector"
+};
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
+//  数据库配置
+const db = require("./config/keys").mongoURI;
+
+// 链接到mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// 首页路由
 app.get("/", (req, res) => res.send("Hello dfd"));
 
 const port = process.env.PORT || 5000;
