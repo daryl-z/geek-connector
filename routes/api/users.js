036a70095fb66af39b37b139d3473e46cef375f5
zzz <angelks4@live.com>
2018-04-18 09:11:06 +0800

    login begin

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route GET api/users/login
+// @desc Login User /Returning JWT Token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // 通过email在数据库中寻找用户
+  User.findOne({ email }).then(user => {
+    // 用户是否存在
+    if (!user) {
+      return res.status(404).json({ email: "用户不存在" });
+    }
+    // 验证密码
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "密码不正确" });
+      }
+    });
+  });
+});
+
 module.exports = router;
