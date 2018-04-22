304aec8a6fca82509a6d823d3f2c49bab318f2c9
Angelki <angelks4@live.com>
2018-04-22 12:42:30 +0800

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import { Button } from "antd";
 
+import BasicLayout from "./components/layout/BasicLayout";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Geek Player</h1>
-        <div>
-          <Button type="primary">Primary</Button>
-          <Button>Default</Button>
-          <Button type="dashed">Dashed</Button>
-          <Button type="danger">Danger</Button>
-        </div>
+        <BasicLayout />
       </div>
     );
   }
