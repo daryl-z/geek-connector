9ffaede30b3290dab861853964be2bb70d455ef5
Angelki <angelks4@live.com>
2018-04-23 14:14:49 +0800

    register layout completed

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,14 +1,152 @@
 import React, { Component } from "react";
+import { Form, Input, Tooltip, Icon, Checkbox, Button, Layout } from "antd";
+const FormItem = Form.Item;
+const { Content } = Layout;
 // import PropTypes from "prop-types";
 // import { connect } from "react-redux";
-
 class Register extends Component {
+  state = {
+    confirmDirty: false
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("两次输入的密码不一致！");
+    } else {
+      callback();
+    }
+  };
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 9 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 6 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 16,
+          offset: 9
+        }
+      }
+    };
+
     return (
-      <div>
-        <h1>Register</h1>
-      </div>
+      <Content style={{ padding: "0 50px" }}>
+        <Layout style={{ padding: "24px 0", background: "#fff" }}>
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem
+              {...formItemLayout}
+              label={
+                <span>
+                  用户昵称&nbsp;
+                  <Tooltip title="其他用户如何称呼您?">
+                    <Icon type="question-circle-o" />
+                  </Tooltip>
+                </span>
+              }
+            >
+              {getFieldDecorator("name", {
+                rules: [
+                  {
+                    required: true,
+                    message: "请输入您的用户昵称!",
+                    whitespace: true
+                  }
+                ]
+              })(<Input />)}
+            </FormItem>
+
+            <FormItem {...formItemLayout} label="邮箱">
+              {getFieldDecorator("email", {
+                rules: [
+                  {
+                    type: "email",
+                    message: "当前输入不合法！"
+                  },
+                  {
+                    required: true,
+                    message: "请输入您的邮箱！"
+                  }
+                ]
+              })(<Input />)}
+            </FormItem>
+
+            <FormItem {...formItemLayout} label="密码">
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    required: true,
+                    message: "请输入密码！"
+                  },
+                  {
+                    validator: this.validateToNextPassword
+                  }
+                ]
+              })(<Input type="password" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="确认密码">
+              {getFieldDecorator("password2", {
+                rules: [
+                  {
+                    required: true,
+                    message: "请确认您的密码！"
+                  },
+                  {
+                    validator: this.compareToFirstPassword
+                  }
+                ]
+              })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
+            </FormItem>
+            <FormItem {...tailFormItemLayout}>
+              {getFieldDecorator("agreement", {
+                valuePropName: "checked"
+              })(
+                <Checkbox>
+                  我已经阅读并同意
+                  <a href="javacript:;">《经常被无视的协议》</a>
+                </Checkbox>
+              )}
+            </FormItem>
+            <FormItem {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+            </FormItem>
+          </Form>
+        </Layout>
+      </Content>
     );
   }
 }
-export default Register;
+export default Form.create()(Register);
