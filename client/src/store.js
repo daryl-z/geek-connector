88d362bbb6456285ec6c7d2ccdf836aa25e8b8e5
Angelki <angelks4@live.com>
2018-04-25 20:28:46 +0800

    add   redux dev tools extension

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 const middleware = [thunk];
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
