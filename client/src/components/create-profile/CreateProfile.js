8bcd81b154680ab0773cadf5826d4a9d11a4b93d
Angelki <angelks4@live.com>
2018-04-28 12:28:45 +0800

    feat(create profile form style)

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -14,41 +14,60 @@ import {
   Col,
   Checkbox,
   Button,
-  AutoComplete
+  AutoComplete,
+  Layout
 } from "antd";
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 const AutoCompleteOption = AutoComplete.Option;
+const { TextArea } = Input;
+const { Content } = Layout;
 
 const residences = [
   {
-    value: "zhejiang",
-    label: "Zhejiang",
+    value: "anhui",
+    label: "安徽",
     children: [
       {
-        value: "hangzhou",
-        label: "Hangzhou",
+        value: "maanshan",
+        label: "马鞍山",
         children: [
           {
-            value: "xihu",
-            label: "West Lake"
+            value: "ahut",
+            label: "安徽工业大学"
           }
         ]
       }
     ]
   },
   {
-    value: "jiangsu",
-    label: "Jiangsu",
+    value: "zhejiang",
+    label: "浙江",
     children: [
       {
-        value: "nanjing",
-        label: "Nanjing",
+        value: "hangzhou",
+        label: "杭州",
         children: [
           {
-            value: "zhonghuamen",
-            label: "Zhong Hua Men"
+            value: "xihu",
+            label: "西湖"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    value: "上海",
+    label: "上海",
+    children: [
+      {
+        value: "changningqu",
+        label: "长宁区",
+        children: [
+          {
+            value: "hongqiao",
+            label: "虹桥"
           }
         ]
       }
@@ -99,6 +118,11 @@ class CreateProfile extends Component {
     }
     this.setState({ autoCompleteResult });
   };
+
+  handleChange = value => {
+    console.log(`selected ${value}`);
+  };
+
   render() {
     const { getFieldDecorator } = this.props.form;
     const { autoCompleteResult } = this.state;
@@ -125,150 +149,253 @@ class CreateProfile extends Component {
         }
       }
     };
-    const prefixSelector = getFieldDecorator("prefix", {
-      initialValue: "86"
-    })(
-      <Select style={{ width: 70 }}>
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    );
 
     const websiteOptions = autoCompleteResult.map(website => (
       <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
     ));
 
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormItem {...formItemLayout} label="E-mail">
-          {getFieldDecorator("email", {
-            rules: [
-              {
-                type: "email",
-                message: "The input is not valid E-mail!"
-              },
-              {
-                required: true,
-                message: "Please input your E-mail!"
+      <Content style={{ padding: "0 50px" }}>
+        <Layout style={{ padding: "24px 0", background: "#fff" }}>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <h1>修改个人信息</h1>
+          </div>
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem
+              {...formItemLayout}
+              label={
+                <span>
+                  简介名称&nbsp;
+                  <Tooltip title="作为简介的唯一标识">
+                    <Icon type="question-circle-o" />
+                  </Tooltip>
+                </span>
               }
-            ]
-          })(<Input />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Password">
-          {getFieldDecorator("password", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your password!"
-              },
-              {
-                validator: this.validateToNextPassword
-              }
-            ]
-          })(<Input type="password" />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Confirm Password">
-          {getFieldDecorator("confirm", {
-            rules: [
-              {
-                required: true,
-                message: "Please confirm your password!"
-              },
-              {
-                validator: this.compareToFirstPassword
-              }
-            ]
-          })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label={
-            <span>
-              Nickname&nbsp;
-              <Tooltip title="What do you want others to call you?">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>
-          }
-        >
-          {getFieldDecorator("nickname", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your nickname!",
-                whitespace: true
-              }
-            ]
-          })(<Input />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Habitual Residence">
-          {getFieldDecorator("residence", {
-            initialValue: ["zhejiang", "hangzhou", "xihu"],
-            rules: [
-              {
-                type: "array",
-                required: true,
-                message: "Please select your habitual residence!"
-              }
-            ]
-          })(<Cascader options={residences} />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Phone Number">
-          {getFieldDecorator("phone", {
-            rules: [
-              { required: true, message: "Please input your phone number!" }
-            ]
-          })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
-        </FormItem>
-        <FormItem {...formItemLayout} label="Website">
-          {getFieldDecorator("website", {
-            rules: [{ required: true, message: "Please input website!" }]
-          })(
-            <AutoComplete
-              dataSource={websiteOptions}
-              onChange={this.handleWebsiteChange}
-              placeholder="website"
             >
-              <Input />
-            </AutoComplete>
-          )}
-        </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="Captcha"
-          extra="We must make sure that your are a human."
-        >
-          <Row gutter={8}>
-            <Col span={12}>
-              {getFieldDecorator("captcha", {
-                rules: [
-                  {
-                    required: true,
-                    message: "Please input the captcha you got!"
-                  }
-                ]
-              })(<Input />)}
-            </Col>
-            <Col span={12}>
-              <Button>Get captcha</Button>
-            </Col>
-          </Row>
-        </FormItem>
-        <FormItem {...tailFormItemLayout}>
-          {getFieldDecorator("agreement", {
-            valuePropName: "checked"
-          })(
-            <Checkbox>
-              I have read the <a href="">agreement</a>
-            </Checkbox>
-          )}
-        </FormItem>
-        <FormItem {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </FormItem>
-      </Form>
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("handle", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "请输入简历的名称!",
+                        whitespace: true
+                      }
+                    ]
+                  })(<Input />)}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="工作状态">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("status", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "请选择您的工作状态!",
+                        whitespace: true
+                      }
+                    ]
+                  })(
+                    <Select
+                      defaultValue="lucy"
+                      style={{ width: "100%" }}
+                      onChange={this.handleChange}
+                    >
+                      <Option value="developer">开发者</Option>
+                      <Option value="junior">初级开发者</Option>
+                      <Option value="senior">高级开发者</Option>
+                      <Option value="teacher">教师</Option>
+                      <Option value="student">学生或者是学习中</Option>
+                      <Option value="other">其他</Option>
+                    </Select>
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="公司">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("company", {
+                    rules: []
+                  })(<Input />)}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="个人主页">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("website", {
+                    rules: []
+                  })(
+                    <AutoComplete
+                      dataSource={websiteOptions}
+                      onChange={this.handleWebsiteChange}
+                      placeholder="个人或公司的网址"
+                    >
+                      <Input />
+                    </AutoComplete>
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="居住地">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {" "}
+                  {getFieldDecorator("residence", {
+                    initialValue: ["anhui", "maanshan", "ahut"],
+                    rules: [
+                      {
+                        type: "array",
+                        required: true,
+                        message: "Please select your habitual residence!"
+                      }
+                    ]
+                  })(<Cascader options={residences} />)}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="技能">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("skills", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "请输入您的技能!",
+                        whitespace: true
+                      }
+                    ]
+                  })(<Input />)}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem
+              {...formItemLayout}
+              label={
+                <span>
+                  GitHub用户名&nbsp;
+                  <Tooltip title="GitHub URL的最后一个路径?">
+                    <Icon type="question-circle-o" />
+                  </Tooltip>
+                </span>
+              }
+            >
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("githubname", {
+                    rules: []
+                  })(<Input style={{ width: "100%" }} />)}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="个人简介">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("bio", {
+                    rules: [
+                      {
+                        message: "请输入您的个人简介!",
+                        whitespace: true
+                      }
+                    ]
+                  })(<TextArea placeholder="对自己的一个简单的介绍" />)}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="微博">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("weibo", {
+                    rules: []
+                  })(
+                    <Input
+                      addonBefore={<Icon type="weibo" />}
+                      style={{ width: "100%" }}
+                    />
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="微信">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("wechat", {
+                    rules: []
+                  })(
+                    <Input
+                      addonBefore={<Icon type="wechat" />}
+                      style={{ width: "100%" }}
+                    />
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>{" "}
+            <FormItem {...formItemLayout} label="bilibili">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("bilibili", {
+                    rules: []
+                  })(
+                    <Input
+                      addonBefore={<Icon type="youtube" />}
+                      style={{ width: "100%" }}
+                    />
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="stack overflow">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("stackoverflow", {
+                    rules: []
+                  })(
+                    <Input
+                      addonBefore={<Icon type="codepen-circle" />}
+                      style={{ width: "100%" }}
+                    />
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...formItemLayout} label="leetcode">
+              <Row gutter={8}>
+                <Col span={12}>
+                  {getFieldDecorator("leetcode", {
+                    rules: []
+                  })(
+                    <Input
+                      addonBefore={<Icon type="code" />}
+                      style={{ width: "100%" }}
+                    />
+                  )}
+                </Col>
+                <Col span={12} />
+              </Row>
+            </FormItem>
+            <FormItem {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                提交
+              </Button>
+            </FormItem>
+          </Form>
+        </Layout>
+      </Content>
     );
   }
 }
