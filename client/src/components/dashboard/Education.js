b35d1e5eff66187aaa344d46b541709e3d5545fb
angleki <angelks4@live.com>
2018-04-30 13:54:43 +0800

    feat(education):education table

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,20 +2,28 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { deleteEducation } from "../../actions/profileActions";
-import { Divider, Table, Icon } from "antd";
+import { Table, Popconfirm, message, Button } from "antd";
 import Moment from "react-moment";
 
 class Education extends Component {
-  onDeleteClick = id => {
-    this.props.deleteEducation(id);
-  };
+  // onDeleteClick = id => {
+  //   this.props.deleteEducation(id);
+  // };
   render() {
+    const { deleteEducation } = this.props;
+    const popContent = "你确定要删除此项吗?";
+
+    function confirm(id) {
+      deleteEducation(id);
+      message.info("删除成功");
+    }
+
     const columns = [
       {
         title: "学校",
         dataIndex: "school",
         key: "school",
-        render: text => <a href="javascript:;">{text}</a>
+        render: text => <a href="">{text}</a>
       },
       {
         title: "学位",
@@ -25,54 +33,54 @@ class Education extends Component {
       {
         title: "起止日期",
         dataIndex: "years",
-        key: "years"
-        // render: text => (
-        //   <div>
-        //     <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
-        //     {edu.to === null ? (
-        //       " Now"
-        //     ) : (
-        //       <Moment format="YYYY/MM/DD">{edu.to}</Moment>
-        //     )}
-        //   </div>
-        // )
+        key: "years",
+        render: (text, record, index) => (
+          <div>
+            <Moment format="YYYY/MM/DD">{text.from}</Moment> -
+            {text.to === null ? (
+              " Now"
+            ) : (
+              <Moment format="YYYY/MM/DD">{text.to}</Moment>
+            )}
+          </div>
+        )
       },
       {
         title: "操作",
         key: "action",
-        render: (text, record) => (
+        render: (text, record, index) => (
           <span>
-            <a href="javascript:;">删除</a>
+            {/* <a
+              href="javascript:;"
+              onClick={() => this.onDeleteClick(text.action)}
+            > */}
+            <Popconfirm
+              placement="left"
+              title={popContent}
+              onConfirm={() => confirm(text.action)}
+              okText="是"
+              cancelText="否"
+            >
+              <Button>删除</Button>
+            </Popconfirm>
           </span>
         )
       }
     ];
-    const data = [
-      {
-        key: "1",
-        name: "John Brown",
-        age: 32,
-        address: "New York No. 1 Lake Park"
-      },
-      {
-        key: "2",
-        name: "Jim Green",
-        age: 42,
-        address: "London No. 1 Lake Park"
-      },
-      {
-        key: "3",
-        name: "Joe Black",
-        age: 32,
-        address: "Sidney No. 1 Lake Park"
-      }
-    ];
+
+    const education = this.props.education.map(edu => ({
+      school: edu.school,
+      degree: edu.degree,
+      years: [edu.from, edu.to],
+      action: edu._id,
+      key: edu._id
+    }));
 
     return (
       <div style={{ margin: "0 20px" }}>
         <Table
           columns={columns}
-          dataSource={data}
+          dataSource={education}
           title={() => <h2>教育经历</h2>}
         />
       </div>
