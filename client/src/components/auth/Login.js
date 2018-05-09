6f8e356037ea8c2e36cd3c038c5d6d851015462c
angleki <angelks4@live.com>
2018-05-09 23:29:02 +0800

    fix(login): password length

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -111,7 +111,14 @@ class Login extends Component {
             </FormItem>
             <FormItem {...formItemLayout} label="邮箱">
               {getFieldDecorator("email", {
-                rules: [{ required: true, message: "请输入您的邮箱！" }]
+                rules: [
+                  { required: true, message: "请输入您的邮箱！" },
+                  {
+                    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/,
+                    message:
+                      "至少8-16个字符，至少1个大写字母，1个小写字母和1个数字"
+                  }
+                ]
               })(
                 <Input
                   prefix={
