f5059aeb62fa715ba134ac5e1332e2043b862c9f
angleki <angelks4@live.com>
2018-05-13 18:46:51 +0800

    feat(mycloud): get tags from backend

--- client/src/components/tagcloud/TagCloud.js
+++ client/src/components/tagcloud/TagCloud.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import _ from "lodash";
 import randomColor from "randomcolor";
 import TagCloud from "react-tag-cloud";
 import CloudItem from "./CloudItem";
 import { Layout, Col, Row, Card, Icon } from "antd";
+import {
+  setCurrentData,
+  setCurrentPage,
+  getPosts
+} from "../../actions/postActions";
 import "./style.css";
 
 const { Content } = Layout;
@@ -25,7 +33,36 @@ class MyCloud extends Component {
     }, 3000);
   }
 
+  componentWillMount() {
+    this.props.getPosts();
+  }
+
+  // 扁平化数组
+  flatten = arr =>
+    arr.reduce(
+      (prev, item) =>
+        prev.concat(Array.isArray(item) ? this.flatten(item) : item),
+      []
+    );
+
+  // 获取所有的tag
+  getAllTags = () => {
+    let tagList = Array.of();
+    this.props.post.posts.map(item => tagList.push(item.tags));
+    return _.uniq(this.flatten(tagList));
+  };
+
   render() {
+    const { posts } = this.props.post;
+    let allTags = this.getAllTags();
+    const tagList = [];
+    for (let i = 0; i < allTags.length; i++) {
+      tagList.push(
+        <div value={allTags[i]} key={i.toString() + i}>
+          {allTags[i]}
+        </div>
+      );
+    }
     return (
       <Content style={{ padding: "0 50px" }}>
         <Layout style={{ padding: "24px 0", background: "#fff" }}>
@@ -35,7 +72,6 @@ class MyCloud extends Component {
           <Card
             bordered={false}
             style={{ margin: "0 20px", minHeight: "800px" }}
-            extra={<Icon type="ellipsis" />}
             title={
               <div style={{ display: "flex", justifyContent: "center" }}>
                 <h4>标签云</h4>
@@ -56,57 +92,7 @@ class MyCloud extends Component {
                     padding: 5
                   }}
                 >
-                  <div
-                    style={{
-                      fontFamily: "serif",
-                      fontSize: 40,
-                      fontStyle: "italic",
-                      fontWeight: "bold",
-                      color: randomColor()
-                    }}
-                  >
-                    Futurama
-                  </div>
-                  <CloudItem text="Custom item, Hover me!" />
-                  <CloudItem text="Custom item 2, Hover me!" />
-                  <div style={styles.large}>Transformers</div>
-                  <div style={styles.large}>Simpsons</div>
-                  <div style={styles.large}>Dragon Ball</div>
-                  <div style={styles.large}>Rick & Morty</div>
-                  <div style={{ fontFamily: "courier" }}>He man</div>
-                  <div style={{ fontSize: 30 }}>World trigger</div>
-                  <div style={{ fontStyle: "italic" }}>Avengers</div>
-                  <div style={{ fontWeight: 200 }}>Family Guy</div>
-                  <div style={{ color: "green" }}>American Dad</div>
-                  <div className="tag-item-wrapper">
-                    <div>Hover Me Please!</div>
-                    <div className="tag-item-tooltip">HOVERED!</div>
-                  </div>
-                  <div>Gobots</div>
-                  <div>Thundercats</div>
-                  <div>M.A.S.K.</div>
-                  <div>GI Joe</div>
-                  <div>Inspector Gadget</div>
-                  <div>Bugs Bunny</div>
-                  <div>Tom & Jerry</div>
-                  <div>Cowboy Bebop</div>
-                  <div>Evangelion</div>
-                  <div>Bleach</div>
-                  <div>GITS</div>
-                  <div>Pokemon</div>
-                  <div>She Ra</div>
-                  <div>Fullmetal Alchemist</div>
-                  <div>Gundam</div>
-                  <div>Uni Taisen</div>
-                  <div>Pinky and the Brain</div>
-                  <div>Bobs Burgers</div>
-                  <div style={styles.small}>Dino Riders</div>
-                  <div style={styles.small}>Silverhawks</div>
-                  <div style={styles.small}>Bravestar</div>
-                  <div style={styles.small}>Starcom</div>
-                  <div style={styles.small}>Cops</div>
-                  <div style={styles.small}>Alfred J. Kwak</div>
-                  <div style={styles.small}>Dr Snuggles</div>
+                  {tagList}
                 </TagCloud>
               </div>
             </div>
@@ -117,4 +103,17 @@ class MyCloud extends Component {
   }
 }
 
-export default MyCloud;
+MyCloud.propTypes = {
+  setCurrentData: PropTypes.func.isRequired,
+  setCurrentPage: PropTypes.func.isRequired,
+  getPosts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+export default connect(mapStateToProps, {
+  setCurrentData,
+  setCurrentPage,
+  getPosts
+})(withRouter(MyCloud));
